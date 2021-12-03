dc6c8435483629bc7ac3a9c42e627e1b60b9a7fd
Freyam Mehta <frey77mehta@gmail.com>
2021-12-03 23:30:12 +0400

    SitBit 1.0
    Co-authored-by: Pratyay Suvarnapathaki <spratyay@gmail.com>
    Co-authored-by: Aaron Monis <pandaZGV2@users.noreply.github.com>

--- app.py
+++ app.py
@@ -14,28 +14,6 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/alert")
-def alert():
-    alerts = {
-        0: "Slouching for too long.",
-        1: "Sitting for too long.",
-        2: "Temperature and/or humidity too high.",
-        3: "Currently Not Seated.",
-        4: "Currently Slouching",
-        5: "You're doing great! Maintain that excellent posture!",
-    }
-
-    uri = f"http://127.0.0.1:8080/~/in-cse/in-name/Alerts/node1/la"
-    headers = {"X-M2M-Origin": "admin:admin", "Content-type": "application/json"}
-    response = requests.get(uri, headers=headers)
-
-    data = json.loads(response.text)
-    alert_id = data["m2m:cin"]["con"]
-    alert = alerts[int(alert_id)]
-
-    return alert
-
-
 @app.route("/data")
 def data():
     import json
@@ -70,7 +48,25 @@ def data():
         plt.savefig(f"static/{sensor}.jpg")
         plt.clf()
 
-    return "end"
+    alerts = {
+        0: ["Slouching for too long.", "red"],
+        1: ["Sitting for too long.", "red"],
+        2: ["Temperature and/or humidity too high.", "red"],
+        3: ["Currently Not Seated.", "yellow"],
+        4: ["Currently Slouching", "orange"],
+        5: ["You're doing great! Maintain that excellent posture!", "green"],
+    }
+
+    uri = f"http://127.0.0.1:8080/~/in-cse/in-name/Alerts/node1/la"
+    headers = {"X-M2M-Origin": "admin:admin", "Content-type": "application/json"}
+    response = requests.get(uri, headers=headers)
+
+    data = json.loads(response.text)
+    alert_id = data["m2m:cin"]["con"]
+    alert = alerts[int(alert_id)][0]
+    alert_color = alerts[int(alert_id)][1]
+
+    return alert
 
 
 if __name__ == "__main__":
